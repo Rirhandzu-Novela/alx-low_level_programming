8d90e73343045d17664d99e0e2aeb2256277d6e1
Rirhandzu-Novela <rirhandzumahansi@gmail.com>
2023-04-20 15:42:32 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-gcc -c $CFILE > ${CFILE::-2}”.o”
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle,\n");
+	return (0);
+}
