dc20ff613fcfb207b9586fdcd3bb57049f086d10
Rirhandzu-Novela <rirhandzumahansi@gmail.com>
2023-04-20 16:53:17 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry poin
+ * Return: Always 1
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpae, 2015-10-19\n", 59);
+	return (1);
+}
