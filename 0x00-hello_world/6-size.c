4bcee2d0fe7b69ca5d7fb19c67b3abb970802b25
Rirhandzu-Novela <rirhandzumahansi@gmail.com>
2023-04-20 17:42:54 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,9 @@
  */
 int main(void)
 {
-	printf("Size of a char: %lu\n", sizeof(char));
-	printf("Size of an int: %lu\n", sizeof(int));
-	printf("Size of a float: %lu\n", sizeof(float));
-	printf("Size of a double: %lu", sizeof(double));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a double: %lu byte(s)", sizeof(double));
 	return (0);
 }
