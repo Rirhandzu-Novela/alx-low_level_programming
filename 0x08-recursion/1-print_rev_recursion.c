99638f48232667b066cacb45ea51c1de79f51517
Rirhandzu-Novela <rirhandzumahansi@gmail.com>
2023-05-14 23:56:55 -0700

    print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,15 +2,19 @@
 /**
  * _print_rev_recursion - print a string in reverse
  * @s: string to be printed
+ * Return: void
  */
 
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursion(s);
-		++s;
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
+	}
+	else if (!*s)
+	{
+		return;
 	}
 }
 
