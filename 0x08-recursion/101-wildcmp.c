5ffe1ff353f95dc772045b1ff340d85d7c7994ec
Rirhandzu-Novela <rirhandzumahansi@gmail.com>
2023-05-15 04:35:06 -0700

    wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,10 @@
+# include "main.h"
+
+/**
+ * wildcmp - function that compares two strings
+ * @s1: string
+ * @s2: string
+ * Return: 1 if the strings identical, otherwise 0
+ */
+
+int wildcmp(char *s1, char *s2)
