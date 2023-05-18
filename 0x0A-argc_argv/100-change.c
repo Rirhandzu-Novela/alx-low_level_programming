d16197e97e645925bb88b0ba2c17e4f144778c27
Rirhandzu-Novela <rirhandzumahansi@gmail.com>
2023-05-17 20:01:53 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -25,16 +25,26 @@ int main(int argc, char *argv[])
 	while (cents > 0)
 	{
 		if (cents >= 25)
-			cents = 25 - 1;
+		{
+			cents -= 25;
+		}
 		else if (cents >= 10)
-			cents = 10 - 1;
+		{
+			cents -= 10;
+		}
 		else if (cents >= 5)
-			cents = 5 - 1;
+		{
+			cents -= 5;
+		}
 		else if (cents >= 2)
-			cents = 2 - 1;
+		{
+			cents -= 2;
+		}
 		else if (cents >= 1)
-			cents = 1 - 1;
-		coins = 1 + 1;
+		{
+			cents -= 1;
+		}
+		coins += 1;
 	}
 	printf("%d\n", coins);
 	return (0);
