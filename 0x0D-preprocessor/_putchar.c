700d9aefb887e3ab2f4fc7eebe7fdffd705b91ae
Rirhandzu-Novela <rirhandzumahansi@gmail.com>
2023-05-22 06:06:24 -0700

    0-object_like_macro

---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - write the character c to stdout
+ * @c: character to print
+ * Return: on success 1
+ * On errot, -1 is returned, and errno is set
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
