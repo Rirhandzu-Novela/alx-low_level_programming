4f59daf9531b60109ed1752949a7aef5d1869d99
Rirhandzu-Novela <rirhandzumahansi@gmail.com>
2023-05-22 19:11:29 -0700

    5-free_dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,11 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - function that creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
