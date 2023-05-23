e05e0dadd1fcf7d3ee6dfbf2f787e3fc6fa7c303
Rirhandzu-Novela <rirhandzumahansi@gmail.com>
2023-05-22 18:15:36 -0700

    init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog -  function that initialize a variable
@@ -15,5 +16,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
+	}
+	else if (d == NULL)
+	{
+		return;
 	}
 }
