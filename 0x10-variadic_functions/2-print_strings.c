dc43b9f97177cc19de56e6b8662dd1a6245527e1
Rirhandzu-Novela <rirhandzumahansi@gmail.com>
2023-05-25 16:37:17 -0700

    2-print_strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,11 +24,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("(nil");
 		}
-		else
+		else if (s != NULL)
 		{
 			printf("%s", s);
 		}
-		if (i < n - 1 && separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
