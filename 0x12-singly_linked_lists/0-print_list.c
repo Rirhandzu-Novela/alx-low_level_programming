bdccb9420afae60ae115473f09508974f0078e0b
Rirhandzu Novela <rirhandzumahansi@gmail.com>
2023-06-03 07:54:20 -0700

    print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,14 +16,7 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h->str != NULL)
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
-		else
-		{
-			printf("[%d] %s\n", 0, "(nil)");
-		}
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		list++;
 	}
