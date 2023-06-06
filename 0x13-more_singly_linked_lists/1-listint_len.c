ec7dafb7bbc5fda3b034f8150cf40bda9242f880
Rirhandzu Novela <rirhandzumahansi@gmail.com>
2023-06-06 14:14:14 -0700

    listint_len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * listint_len -  function that returns the number of elements in a linked listint_t
+ * @h: pointer
+ * Return: int
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i;
+
+	i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i += 1;
+	}
+	return (i);
+}
